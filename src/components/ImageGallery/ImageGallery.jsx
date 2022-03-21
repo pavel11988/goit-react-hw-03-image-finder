65a150bd08372f6f563e553e2713cc0315708ec1
pavel11988 <kossoua88@gmail.com>
2022-03-21 21:09:03 +0200

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from 'components/Button/Button';
 import ImageError from 'components/ImageError/ImageError';
@@ -11,50 +10,20 @@ import {
   Idle,
 } from './ImageGallery.styled';
 
-export default class ImageGallery extends Component {
-  static propTypes = {
-    getUrlLarge: PropTypes.func.isRequired,
-    imagesArray: PropTypes.arrayOf(PropTypes.object).isRequired,
-    status: PropTypes.string.isRequired,
-    loadMore: PropTypes.func.isRequired,
-    error: PropTypes.object,
-  };
+const ImageGallery = ({
+  getUrlLarge,
+  imagesArray,
+  status,
+  loadMore,
+  error,
+  notLastPage,
+}) => {
+  if (status === 'idle') {
+    return <Idle>Please, enter your request.</Idle>;
+  }
 
-  render() {
-    const { getUrlLarge, imagesArray, status, loadMore, error, notLastPage } =
-      this.props;
-
-    if (status === 'idle') {
-      return <Idle>Please, enter your request.</Idle>;
-    }
-
-    if (status === 'pending') {
-      if (imagesArray.length !== 0) {
-        return (
-          <ImageGalleryContainer>
-            <ImageGalleryList className="gallery">
-              {imagesArray.map(item => (
-                <ImageGalleryItem
-                  key={item.id}
-                  webformatURL={item.webformatURL}
-                  large={item.largeImageURL}
-                  tags={item.tags}
-                  getUrlLarge={getUrlLarge}
-                />
-              ))}
-            </ImageGalleryList>
-            <Loader />
-          </ImageGalleryContainer>
-        );
-      }
-      if (imagesArray.length === 0) return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return <ImageError message={error.message} />;
-    }
-
-    if (status === 'resolved') {
+  if (status === 'pending') {
+    if (imagesArray.length !== 0) {
       return (
         <ImageGalleryContainer>
           <ImageGalleryList className="gallery">
@@ -64,14 +33,48 @@ export default class ImageGallery extends Component {
                 webformatURL={item.webformatURL}
                 large={item.largeImageURL}
                 tags={item.tags}
-                getUrlLarge={this.props.getUrlLarge}
+                getUrlLarge={getUrlLarge}
               />
             ))}
           </ImageGalleryList>
-
-          {notLastPage && <Button loadMore={loadMore} />}
+          <Loader />
         </ImageGalleryContainer>
       );
     }
+    if (imagesArray.length === 0) return <Loader />;
   }
-}
+
+  if (status === 'rejected') {
+    return <ImageError message={error.message} />;
+  }
+
+  if (status === 'resolved') {
+    return (
+      <ImageGalleryContainer>
+        <ImageGalleryList className="gallery">
+          {imagesArray.map(item => (
+            <ImageGalleryItem
+              key={item.id}
+              webformatURL={item.webformatURL}
+              large={item.largeImageURL}
+              tags={item.tags}
+              getUrlLarge={getUrlLarge}
+            />
+          ))}
+        </ImageGalleryList>
+
+        {notLastPage && <Button loadMore={loadMore} />}
+      </ImageGalleryContainer>
+    );
+  }
+};
+
+ImageGallery.propTypes = {
+  getUrlLarge: PropTypes.func.isRequired,
+  imagesArray: PropTypes.arrayOf(PropTypes.object).isRequired,
+  status: PropTypes.string.isRequired,
+  loadMore: PropTypes.func.isRequired,
+  error: PropTypes.object,
+};
+
+export default ImageGallery;
